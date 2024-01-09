0cd6f9658e6388175611c8fadf8b595df0f6c064
pennymax <pennymaxlw@gmail.com>
2024-01-09 16:19:40 +0800

    clean extra_fitness file

--- gplearn/extra_fitness.py
+++ gplearn/extra_fitness.py
@@ -4,235 +4,14 @@ import pandas as pd
 from .fitness import _Fitness
 
 
-_annulization = 365 * 3 #默认8h  #TODO
-_fee = 0.001
+_bad_fitness_val = -1000
+_annual_bar_8h = 365 * 3
+_fee_rate = 0.001
 
-def is_bad_data(y_pred, thresh=0.7):
+def is_bad_data(y_pred, thresh=0.3):
     total_cs = y_pred.shape[0]
     all_na_cs = np.sum(np.all(np.isnan(y_pred), axis=1))
     return all_na_cs / total_cs >= thresh
-
-
-def compute_IC(y, y_pred, w, rank_ic=True):
-    if is_bad_data(y_pred):
-        return 0
-    y = y[w.astype(bool)]
-    y_pred = y_pred[w.astype(bool)]
-    if rank_ic:
-        ic = pd.DataFrame(y_pred).corrwith(pd.DataFrame(y),axis = 1, method = "spearman").fillna(0)  # use fillna(0) to avoid getting high corr value if majorities are NaN
-    else:
-        ic = pd.DataFrame(y_pred).corrwith(pd.DataFrame(y),axis = 1, method = "pearson").fillna(0)
-    return ic 
-
-def _rank_IC(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    ic = compute_IC(y, y_pred, w).mean()
-    if np.isnan(ic):
-        return 0
-    else:
-        return abs(ic)
-
-def _rank_ICIR(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    ics = compute_IC(y, y_pred, w)
-    ic = ics.mean()
-    ic_std = ics.std()
-    icir = ic / ic_std
-    if np.isnan(icir):
-        return 0
-    else:
-        return abs(icir)
-    
-def compute_quantile_rets_ori(y, y_pred, w, quantiles):
-    y_pred = y_pred[w.astype(bool)]
-    y = y[w.astype(bool)]
-    if np.all(np.isnan(y_pred)):
-        return None
-    
-    groups = np.array(range(quantiles)) + 1
-    
-    try:
-        factor_quantiles = pd.DataFrame(y_pred).rank(axis=1,method='first').dropna(axis=0, how='all').apply(pd.qcut, q=quantiles, labels=groups, axis=1, duplicates='drop')
-    except:
-        return None
-
-    rets = pd.DataFrame(y)
-    return_series = {}
-    for group in groups:
-        returns_group = rets[factor_quantiles == group]
-        return_series[group] = (returns_group.sum(axis=1) / returns_group.count(axis=1)).mean() * _annulization # scale holding to 1 ; equal weights
-    return return_series
-
-def turnover_rate(df):
-    df = df.notna()
-    per_bar_changes = df.diff().abs().sum(axis=1) / 2
-    per_bar_count = df.sum(axis=1)
-    rates = per_bar_changes / per_bar_count.shift()
-    avg_rate = rates.mean()
-    return avg_rate
-
-def compute_quantile_rets(y, y_pred, w, quantiles):
-    y_pred = y_pred[w.astype(bool)]
-    y = y[w.astype(bool)]
-    if np.all(np.isnan(y_pred)):
-        return None, None
-    
-    rets = pd.DataFrame(y)
-    factor = pd.DataFrame(y_pred)
-
-    ## use y (return) to mask y_pred to set 0 on all invaid cells to nan
-    factor = factor.mask(rets.isna())
-    
-    groups = np.array(range(quantiles)) + 1
-    
-    try:
-        factor_quantiles = (
-            factor
-            .rank(axis=1, method='first') # method first means assign different ranks on identical values
-            .dropna(axis=0, how='all')
-            .apply(pd.qcut, q=quantiles, labels=groups, axis=1, duplicates='drop')
-            )
-    except:
-        return None, None
-    
-    stacked_rets = rets.stack()
-    stacked_factor_quantiles = factor_quantiles.stack()
-    grouped_returns = (
-        stacked_rets
-        .groupby([stacked_rets.index.get_level_values(0), stacked_factor_quantiles])
-        .mean()
-        .unstack()
-        .mean()
-        * _annulization
-        ) 
-    return grouped_returns, factor_quantiles
-
-def compute_quantile_rets_fast(y, y_pred, w, quantiles):
-    y_pred = y_pred[w.astype(bool)]
-    y = y[w.astype(bool)]
-    if np.all(np.isnan(y_pred)):
-        return None, None
-    
-    rets = pd.DataFrame(y)
-    factor = pd.DataFrame(y_pred)
-
-    ## use y (return) to mask y_pred to set 0 on all invaid cells to nan
-    factor = factor.mask(rets.isna())
-    
-    ret_qtl = []
-    for _, grp in factor.stack().dropna().groupby(level=0, group_keys=True):
-        ranks = grp.rank(method='first')   # method first means assign different ranks on identical values
-        bins = np.linspace(ranks.min(), ranks.max(), quantiles+1)
-        qtl = np.searchsorted(bins, ranks, side='left')
-        qtl[qtl==0] = 1
-        ret_qtl.append(pd.Series(qtl, index=ranks.index))
-    if not ret_qtl:
-        return None, None
-    factor_quantiles = pd.concat(ret_qtl)
-
-    stacked_rets = rets.stack()
-    stacked_factor_quantiles = factor_quantiles
-    grouped_returns = (
-        stacked_rets
-        .groupby([stacked_rets.index.get_level_values(0), stacked_factor_quantiles])
-        .mean()
-        .unstack()
-        .mean()
-        * _annulization
-        ) 
-    return grouped_returns, factor_quantiles.unstack()
-
-def _quantile35_max(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    
-    res, _ = compute_quantile_rets_fast(y, y_pred, w, 35)
-    if res is None:
-        return 0
-    else:
-        return max(res.values)
-    
-def measure_monotonicity(data):
-    ranks = [sorted(data).index(x) + 1 for x in data]
-    rank_differences = [ranks[i] - ranks[i-1] for i in range(1, len(ranks))]
-    positive_differences = sum(1 for diff in rank_differences if diff > 0)
-    negative_differences = sum(1 for diff in rank_differences if diff < 0)
-    monotonicity_score = abs(positive_differences - negative_differences) / len(data)
-    return monotonicity_score
-
-def _quantile35_monotonicity(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    
-    res, _ = compute_quantile_rets_fast(y, y_pred, w, 35)
-    if res is None:
-        return 0
-    else:
-        return measure_monotonicity(res.values)
-
-def _quantile35_longshort(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    
-    quantiles = 35
-    res, _ = compute_quantile_rets_fast(y, y_pred, w, quantiles)
-    if res is None:
-        return 0
-    else:
-        ret = abs(res[quantiles] - res[1]) / 2 # annualized longshort (not full time span）
-        if np.isnan(ret):
-            return 0
-        else:
-            return ret
-
-def _quantile35_longshort_fee(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    
-    quantiles = 35
-    res, factor_quantiles = compute_quantile_rets(y, y_pred, w, quantiles)
-    if res is None or factor_quantiles is None:
-        return 0
-    else:
-        if quantiles not in res.index or 1 not in res.index:
-            return 0
-        ret = abs(res[quantiles] - res[1]) / 2 # annualized longshort (not full time span）
-        if np.isnan(ret):
-            return 0
-        else:
-            long_turnover = turnover_rate(factor_quantiles[factor_quantiles == quantiles])
-            short_turnover = turnover_rate(factor_quantiles[factor_quantiles == 1])
-            avg_turnover = (long_turnover + short_turnover) / 2
-            ret_fee = ret - avg_turnover * _fee * _annulization
-            return ret_fee
-
-def _quantile35_longshort_fee_fast(y, y_pred, w):
-    if is_bad_data(y_pred):
-        return 0
-    
-    quantiles = 35
-    res, factor_quantiles = compute_quantile_rets_fast(y, y_pred, w, quantiles)
-    if res is None or factor_quantiles is None:
-        return 0
-    else:
-        if quantiles not in res.index or 1 not in res.index:
-            return 0
-        ret = abs(res[quantiles] - res[1]) / 2 # annualized longshort (not full time span）
-        if np.isnan(ret):
-            return 0
-        else:
-            long_turnover = turnover_rate(factor_quantiles[factor_quantiles == quantiles])
-            short_turnover = turnover_rate(factor_quantiles[factor_quantiles == 1])
-            avg_turnover = (long_turnover + short_turnover) / 2
-            ret_fee = ret - avg_turnover * _fee * _annulization
-            return ret_fee
-
-
-###################################
-        
-_bad_fitness_val = -1000
 
 def quantile_returns_and_groups(y, y_pred, quantile):
     fwdrets = pd.DataFrame(y)
@@ -459,53 +238,50 @@ def win_rate(returns):
 ##### Finess wrapper
 ##################################
 
-annual_bar_8h = 365 * 3
-fee_rate = 0.001
-
 ## total returns
 def fitness_quantile35_longshort_total_return_cumprod_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     return total_return(longshort_rets, comp=True)
 
 def fitness_quantile35_longshort_total_return_cumsum_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     return total_return(longshort_rets, comp=False)
 
 ## cagr
 def fitness_quantile35_longshort_cagr_cumprod_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return cagr(longshort_rets, comp=True, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return cagr(longshort_rets, comp=True, annual_bars=_annual_bar_8h)
 
 def fitness_quantile35_longshort_cagr_cumsum_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return cagr(longshort_rets, comp=False, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return cagr(longshort_rets, comp=False, annual_bars=_annual_bar_8h)
 
 def fitness_quantile35_longshort_cagr_cumprod(y, y_pred, w):
     longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=0)
-    return cagr(longshort_rets, comp=True, annual_bars=annual_bar_8h)
+    return cagr(longshort_rets, comp=True, annual_bars=_annual_bar_8h)
 
 def fitness_quantile35_longshort_cagr_cumsum(y, y_pred, w):
     longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=0)
-    return cagr(longshort_rets, comp=False, annual_bars=annual_bar_8h)
+    return cagr(longshort_rets, comp=False, annual_bars=_annual_bar_8h)
 
 ## sharpe
 def fitness_quantile35_longshort_sharpe_fine_cumprod_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return sharpe_fine(longshort_rets, comp=True, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return sharpe_fine(longshort_rets, comp=True, annual_bars=_annual_bar_8h)
 
 def fitness_quantile35_longshort_sharpe_fine_cumsum_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return sharpe_fine(longshort_rets, comp=False, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return sharpe_fine(longshort_rets, comp=False, annual_bars=_annual_bar_8h)
 
 ## rolling sharpe sharpe
 def fitness_quantile35_longshort_sharpe_simple_cumprod_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return sharpe_simple(longshort_rets, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return sharpe_simple(longshort_rets, annual_bars=_annual_bar_8h)
 
 def fitness_quantile35_longshort_rolling_sharpe_sharpe_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     # default simple sharpe and half year
-    return rolling_sharpe_sharpe(longshort_rets, window=int(annual_bar_8h/2), annual_bars=annual_bar_8h)
+    return rolling_sharpe_sharpe(longshort_rets, window=int(_annual_bar_8h/2), annual_bars=_annual_bar_8h)
 
 ## monotonicity
 def fitness_quantile35_monotonicity_with_fee(y, y_pred, w):
@@ -524,22 +300,26 @@ def fitness_quantile35_longshort_avg_turnover_rate(y, y_pred, w):
 
 ## mdd
 def fitness_quantile35_longshort_cumprod_mdd_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     return quantile_longshort_dd_top_n_mean(longshort_rets, 1)
 
 def fitness_quantile35_longshort_cumprod_top5_avg_mdd_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     return quantile_longshort_dd_top_n_mean(longshort_rets, 5)
 
 ## sortino
 def fitness_quantile35_longshort_sortino_cumprod_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
-    return quantile_longshort_sortino(longshort_rets, annual_bars=annual_bar_8h)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
+    return quantile_longshort_sortino(longshort_rets, annual_bars=_annual_bar_8h)
 
 ## win rate
 def fitness_quantile35_longshort_winrate_with_fee(y, y_pred, w):
-    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=fee_rate)
+    longshort_rets = quantile_longshort_returns(y, y_pred, w, quantile=35, fee_rate=_fee_rate)
     return win_rate(longshort_rets)
+
+## max avg long return
+def fitness_quantile35_longshort_max_avg_long_return_with_fee(y, y_pred, w):
+    ...
     
 
 _extra_fitness_map = {
@@ -591,4 +371,73 @@ _extra_fitness_map = {
 }
 
 
+def batch_calc_metrics(y, y_pred, w, metrics: list, quantile, fee_rate, annual_bars):
+    ret = {}
 
+    ## prepare data
+    grouped_returns, factor_quantiles = quantile_returns_and_groups(y, y_pred, quantile)
+    if grouped_returns.empty or factor_quantiles.empty or quantile not in grouped_returns.columns or 1 not in grouped_returns.columns:
+        return pd.Series()
+    longshort_rets = (grouped_returns[quantile] - grouped_returns[1]) / 2
+    # long
+    pfl = factor_quantiles[factor_quantiles == quantile]
+    long_rates = turnover_rates(pfl)
+    # short
+    pfl = factor_quantiles[factor_quantiles == 1]
+    short_rates = turnover_rates(pfl)
+    longshort_rates = (long_rates + short_rates) / 2
+    longshort_fee = longshort_rates * fee_rate
+    longshort_rets_fee = longshort_rets - longshort_fee
+
+    for metric in metrics:
+        if metric == 'longshort_total_return_cumprod_with_fee':
+            ret[metric] = total_return(longshort_rets_fee, comp=True)
+        elif metric == 'longshort_total_return_cumsum_with_fee':
+            ret[metric] = total_return(longshort_rets_fee, comp=False)
+        elif metric == 'longshort_cagr_cumprod_with_fee':
+            ret[metric] = cagr(longshort_rets_fee, comp=True, annual_bars=annual_bars)
+        elif metric == 'longshort_cagr_cumsum_with_fee':
+            ret[metric] = cagr(longshort_rets_fee, comp=False, annual_bars=annual_bars)
+        elif metric == 'longshort_cagr_cumprod':
+            ret[metric] = cagr(longshort_rets, comp=True, annual_bars=annual_bars)
+        elif metric == 'longshort_cagr_cumsum':
+            ret[metric] = cagr(longshort_rets, comp=False, annual_bars=annual_bars)
+        elif metric == 'longshort_sharpe_fine_cumprod_with_fee':
+            ret[metric] = sharpe_fine(longshort_rets_fee, comp=True, annual_bars=annual_bars)
+        elif metric == 'longshort_sharpe_fine_cumsum_with_fee':
+            ret[metric] = sharpe_fine(longshort_rets_fee, comp=False, annual_bars=annual_bars)
+        elif metric == 'longshort_sharpe_simple_cumprod_with_fee':
+            ret[metric] = sharpe_simple(longshort_rets_fee, annual_bars=annual_bars)
+        elif metric == 'longshort_rolling_sharpe_sharpe_with_fee':
+            ret[metric] = rolling_sharpe_sharpe(longshort_rets_fee, window=int(annual_bars/2), annual_bars=annual_bars)
+        elif metric == 'longshort_cumprod_mdd_with_fee':
+            ret[metric] = quantile_longshort_dd_top_n_mean(longshort_rets_fee, 1)
+        elif metric == 'longshort_cumprod_top5_avg_mdd_with_fee':
+            ret[metric] = quantile_longshort_dd_top_n_mean(longshort_rets_fee, 5)
+        elif metric == 'longshort_sortino_cumprod_with_fee':
+            ret[metric] = quantile_longshort_sortino(longshort_rets_fee, annual_bars=annual_bars)
+        elif metric == 'longshort_winrate_with_fee':
+            ret[metric] = win_rate(longshort_rets_fee)
+        elif metric == 'monotonicity_with_fee':
+            if len(grouped_returns) < 1:
+                ret[metric] = _bad_fitness_val
+            grouped_returns_mean = grouped_returns.mean()
+            ranks = [sorted(grouped_returns_mean).index(x) + 1 for x in grouped_returns_mean]
+            rank_differences = [ranks[i] - ranks[i-1] for i in range(1, len(ranks))]
+            positive_differences = sum(1 for diff in rank_differences if diff > 0)
+            negative_differences = sum(1 for diff in rank_differences if diff < 0)
+            monotonicity_score = abs(positive_differences - negative_differences) / len(grouped_returns_mean) if len(grouped_returns_mean) > 0 else _bad_fitness_val
+            ret[metric] = monotonicity_score
+        elif metric == 'rank_ic':
+            ret[metric] = ic(y, y_pred, w, rank_ic=True)
+        elif metric == 'rank_icir':
+            ret[metric] = icir(y, y_pred, w, rank_ic=True)
+        elif metric == 'longshort_avg_turnover_rate':
+            ret[metric] = longshort_rates.mean()
+        else:
+            print(f'  !! unknown metric {metric}, please check!')
+            return pd.Series()
+    ret = pd.Series(ret)
+    ret.index.name = 'metric'
+    ret.name = 'value'
+    return ret
